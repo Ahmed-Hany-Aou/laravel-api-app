1fc37e8505ad75e6aa88fb50846c52985f4cdeda
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-09-21 20:39:29 +0300

    implemented destroy method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -62,6 +62,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post -> delete();
+        return ['message'=>'The post was deleted'];
     }
 }
